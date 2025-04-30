f00948cccf480a3f26d4306213ac28e2633e86c8
Dhara Patel <dharapatel2151@gmail.com>
2025-05-01 02:07:55 +0530

    atlas

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log("MongoDB connected locally!"))
+.then(() => console.log("MongoDB connected!"))
 .catch((err) => console.error("MongoDB connection error:", err));
 
 // Start server

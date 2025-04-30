7e850c5909286c6f4d38245723d275e28a0a58bc
Dhara Patel <dharapatel2151@gmail.com>
2025-05-01 01:51:39 +0530

    atlas

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/properties', async (req, res) => {
 
 
 // DB Connection
-mongoose.connect(process.env.BACKEND_URL, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })

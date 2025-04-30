e60062698d5c4307e44f5355c0936ecbfd4ff2c4
Dhara Patel <dharapatel2151@gmail.com>
2025-05-01 02:06:19 +0530

    atlas

--- server.js
+++ server.js
@@ -40,5 +40,5 @@ mongoose.connect(process.env.MONGO_URI, {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running`);
 });
